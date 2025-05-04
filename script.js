d9a504f9b39ce458670544a966ad164c05ade7c9
Elainpojatkissa <elainpojatkissa@gmail.com>
2025-05-04 18:01:41 +0300

    Update script.js
    lisätty äänet

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let soundsEnabled = true;
 
 function exploreForest() {
     document.getElementById('fact').innerText = "Tämä metsäpolku vie sinut syvälle taianomaiseen metsään, jossa voit kohdata erilaisia eläimiä ja kasveja.";
-    playSound('forest.mp3');
+    playSound('https://www.soundjay.com/button/beep-07.wav'); // Esimerkki ääni
 }
 
 function exploreRiver() {
     document.getElementById('fact').innerText = "Jokiretki vie sinut virtaavan joen varrelle, jossa voit nähdä upeita vesilintuja ja vesikasveja.";
-    playSound('river.mp3');
+    playSound('https://www.soundjay.com/button/beep-09.wav'); // Esimerkki ääni
 }
 
 function exploreMountain() {
     document.getElementById('fact').innerText = "Vuoriretki vie sinut korkealle vuoristoon, josta avautuu huikeat maisemat ja voit nähdä harvinaisia vuorieläimiä.";
-    playSound('mountain.mp3');
+    playSound('https://www.soundjay.com/button/beep-10.wav'); // Esimerkki ääni
 }
 
 function playSound(sound) {
